be74664df5053760f88be54aa2032d1f80ab2dc4
Niguti <niguti005@gmail.com>
2024-12-04 10:23:42 -0300

    create player.js

---
+++ player.js
@@ -0,0 +1,12 @@
+class Player {
+    constructor(x, y, size, step){ //initial
+        this.x = x
+        this.y = y
+        this.size = size
+        this.step = step
+    }
+
+    move(){}
+    colisionWall(){}
+    draw(){}
+}
