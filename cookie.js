6d3ea46a024f4510f5b06cc073c80473f71adaed
Niguti <niguti005@gmail.com>
2024-12-05 12:59:33 -0300

    cookie element

--- cookie.js
+++ cookie.js
@@ -9,10 +9,18 @@ class Cookie {
 
     createElement(element){
         const cookie = document.createElement('div')
-        cookie.classList.add('cookie')
+        cookie.classList.add(cookie)
+        cookie.style.position = 'absulute';
+        cookie.style.left = `${this.x}px`
+        cookie.style.top = `${this.y}px`
+        cookie.style.width = '100px'
+        cookie.style.height = '100px'
+        cookie.style.background = 'url("https://img.freepik.com/psd-gratuitas/cozinhar-deliciosos-biscoitos-de-chocolate-isolados-em-fundo-transparente_191095-16881.jpg?t=st=1733414280~exp=1733417880~hmac=a5c29a9b1f44a1c93778322624cbd332ff75b7d44d7086b61778b6d3bf42da10&w=740") no-repeat center';
+        cookie.style.cursor = 'pointer'
+        document.body.appendChild(cookie);
         return cookie
     }
 
-    
+
 
 }
