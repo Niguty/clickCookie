8fbeec79c01a015338123dd286f773af47e83c1a
Niguti <niguti005@gmail.com>
2024-12-05 13:45:14 -0300

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const cookie = new Cookie({
-    x: window.innerWidth / 2 - 50, 
-    y: window.innerHeight / 2 - 50, 
-    count: 0
-});
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.innerHTML += '<div id="counter">Cliques: 0</div>';
 
-document.body.innerHTML += '<div id="counter">Cliques: 0</div>';
+    const cookie = new Cookie({
+        x: window.innerWidth / 2 - 50, 
+        y: window.innerHeight / 2 - 50, 
+        count: 0
+    });
+
+    console.log('Aplicação iniciada!');
+});
